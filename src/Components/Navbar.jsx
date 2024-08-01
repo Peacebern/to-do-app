477125d95f1f8ad854d7b9e096811cdec3767cbb
Peacebern <peace8282bern@gmail.com>
2024-08-01 07:35:32 +0100

    fixed outline

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,22 +3,30 @@ import iconMoon from "../Assets/Images/iconMoon.svg"
 
 function Navbar() {
     return (
-        <div className='nav-bg'>
-            <header>
-                <nav className="nav-menu">
-                    <div>
-                        <p className="nav-todo">T O D O</p>
-                    </div>
-                    <div className="nav-img">
-                        <img src={iconMoon} alt="iconMoon" />
-                    </div>
+        <div>
+            <div className='nav-bg'>
+                <header>
+                    <nav className="nav-menu">
+                        <div>
+                            <p className="nav-todo">T O D O</p>
+                        </div>
+                        <div className="nav-img">
+                            <img src={iconMoon} alt="iconMoon" />
+                        </div>
 
-                </nav>
-                <div className="nav-input">
-                    <input type="checkbox" />
-                    <input type="text" id="task-input" placeholder="Create a new todo..." />
-                </div>
-            </header>
+                    </nav>
+                    <div className="nav-input">
+                        <input type="checkbox" />
+                        <input type="text" id="task-input" placeholder="Create a new todo..." />
+                    </div>
+                </header>
+            </div>
+
+            <div>
+                <ul id="todo-list" ></ul>
+
+        
+            </div>
         </div>
 
         
